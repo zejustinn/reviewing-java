e1345dd3058bf5ad75e9af2e5bcaf25001da574d
Jose Leandro Santos Justin <joselsjustin@gmail.com>
2020-08-02 15:05:51 -0300

    Add first project using IntelliJ

---
+++ firstIntelliJProject/src/com/firstintellijproject/Main.java
@@ -0,0 +1,8 @@
+package com.firstintellijproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Testing IntelliJ in a Windows environment");
+    }
+}
